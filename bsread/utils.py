19c9f71ab565fd34864b3b35e9132c16f3e7c0d1
NichtJens <nichtjens@gmail.com>
2022-02-19 20:24:24 +0100

    added some constants

--- bsread/utils.py
+++ bsread/utils.py
@@ -1,18 +1,23 @@
 import re
 
 
+DISPATCHER_ADDR = "https://dispatcher-api.psi.ch/"
+
+DEFAULT_DISPATCHER_URLS = [
+    DISPATCHER_ADDR + "sf-databuffer",
+    DISPATCHER_ADDR + "sf-imagebuffer"
+]
+
+
 def get_base_urls(base_url=None, backend=None):
     # Get the correct base url based on the values give for base_url and/or backend
 
     if base_url is not None:
         base_url = [base_url]
     elif backend is not None:
-        base_url = ["https://dispatcher-api.psi.ch/"+backend]
+        base_url = [DISPATCHER_ADDR + backend]
     else:
-        base_url = [
-            "https://dispatcher-api.psi.ch/sf-databuffer",
-            "https://dispatcher-api.psi.ch/sf-imagebuffer"
-        ]
+        base_url = DEFAULT_DISPATCHER_URLS
 
     return base_url
 
