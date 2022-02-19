fc689dd9bc85227299f8b37b686a203ae9af5501
NichtJens <nichtjens@gmail.com>
2022-02-19 20:29:24 +0100

    added default base_urls

--- bsread/dispatchers.py
+++ bsread/dispatchers.py
@@ -1,13 +1,14 @@
-import bsread.dispatcher as _dispatcher
+from bsread import dispatcher as _dispatcher
+from bsread.utils import DEFAULT_DISPATCHER_URLS
 
 
-def get_current_channel_names(base_urls):
+def get_current_channel_names(base_urls=DEFAULT_DISPATCHER_URLS):
     res = _collect(_dispatcher.get_current_channel_names, base_urls)
     res.sort()
     return res
 
 
-def get_current_channels(base_urls):
+def get_current_channels(base_urls=DEFAULT_DISPATCHER_URLS):
     res = _collect(_dispatcher.get_current_channels, base_urls)
     res.sort(key=lambda ch: ch["name"])
     return res
